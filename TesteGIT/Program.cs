e138f536b23616866193ae54926ffef9b8243057
Pedro Henrique <phphalves@gmail.com>
2021-01-15 06:03:41 -0300

    Projeto teste, versão inicial

---
+++ TesteGIT/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TesteGIT {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
